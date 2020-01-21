47020863f3af91a73a0ba429b523ca4f4b46d5b4
Naters <nate.garza@hotmail.com>
2020-01-20 23:49:55 -0800

    setup homepage w bootstrap

--- app/routes.py
+++ app/routes.py
@@ -3,5 +3,6 @@ from flask import render_template, flash, redirect, url_for
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/home')
+def homepage():
+    return render_template('homepage.html')
