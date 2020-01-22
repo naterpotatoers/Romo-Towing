9908c2ec129e6e7ee3d8cb4e32a232e312e30718
Naters <nate.garza@hotmail.com>
2020-01-21 19:19:11 -0800

    set up basic ui e.g. nav

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,10 @@ from flask import render_template, flash, redirect, url_for
 
 
 @app.route('/')
-@app.route('/home')
-def homepage():
-    return render_template('homepage.html')
+@app.route('/Home')
+def home():
+    return render_template('home.html')
+
+@app.route('/Pricing')
+def pricing():
+    return render_template('pricing.html')
